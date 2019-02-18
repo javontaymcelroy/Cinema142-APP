c579ea76ae21c769dbb323115b8788cdcfef4374
Javontay McElroy <dinomightnetwork@gmail.com>
2019-02-18 00:28:56 -0600

    tweaks to a tag and styling.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ import './Navigation.css';
 const Navigation = props => {
     return (
         <div className="navigation">
-            <a href="index" ><img className="logo" src="https://i.ibb.co/jGNRvPV/logo.png" alt="logo"/></a>
+            <a href="index.html" ><img className="logo" src="https://i.ibb.co/jGNRvPV/logo.png" alt="logo"/></a>
             <form onSubmit={props.submitSearch}><input className="search" onChange={props.searchChangeHandler} placeholder="Search Movies" name="search"/></form>
         </div>
     )
